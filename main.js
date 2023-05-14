568e29b4e816b52b111f25e273e2c5e7fcf8ae07
tsegaye27 <ttalegn29@gmail.com>
2023-05-15 00:37:18 +0300

    script for scroll section active link is completed

--- main.js
+++ main.js
@@ -2,31 +2,26 @@
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
-
 //MENU SHOW
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 }
-
 //MENU HIDDEN
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
 }
-
 //REMOVE MENU MOBILE
 const navLink = document.querySelectorAll(".nav__link");
-
 const linkAction = () => {
   const navMenu = document.getElementById("nav-menu");
   //when we click on each nav__link, we remove the show-menu
   navMenu.classList.remove("show-menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
-
 //ADD BLUR TO HEADER
 const blurHeader = () => {
   const header = document.getElementById("header");
@@ -35,13 +30,10 @@ const blurHeader = () => {
     ? header.classList.add("blur-header")
     : header.classList.remove("blur-header");
 };
-
 window.addEventListener("scroll", blurHeader);
-
 // EMAIL JS
 const contactForm = document.getElementById("contact-form"),
   contactMessage = document.getElementById("contact-message");
-
 const sendEmail = (e) => {
   e.preventDefault();
   //ServiceID - templateID - #form - publicKey
@@ -71,16 +63,34 @@ const sendEmail = (e) => {
       }
     );
 };
-
 contactForm.addEventListener("submit", sendEmail);
-
 //SHOW SCROLL UP
 const scrollUp = () => {
   const scrollUp = document.getElementById("scroll-up");
-
   this.scrollY >= 350
     ? scrollUp.classList.add("show-scroll")
     : scrollUp.classList.remove("show-scroll");
 };
-
 window.addEventListener("scroll", scrollUp);
+//SCROLL SECTION ACTIVE LINK
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
