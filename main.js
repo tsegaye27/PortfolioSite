adcbfe5d353a640c711d57803aa1a91620766e4b
tsegaye27 <ttalegn29@gmail.com>
2023-05-15 10:59:47 +0300

    the script for the scrolling animations is done

--- main.js
+++ main.js
@@ -94,3 +94,20 @@ const scrollActive = () => {
   });
 };
 window.addEventListener("scroll", scrollActive);
+
+//Scroll reveal animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  //reset: true // Animations repeat
+});
+
+sr.reveal(
+  `.home__data, .home__social, .contact__container, .footer__container`
+);
+sr.reveal(`.home__image`, { origin: "bottom" });
+sr.reveal(`.about__data, .skills__data`, { origin: "left" });
+sr.reveal(`.about__image`, { origin: "right" });
+sr.reveal(`.services__card, .projects__card`, { interval: 100 });
